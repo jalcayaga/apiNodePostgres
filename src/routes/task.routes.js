e0137fe72436881beeac8d4f426e9e0402153ab3
Javier Alcayaga <javier.alcayaga@subred.cl>
2023-04-07 03:05:23 -0400

    DB conectada consulta get await pool.query('SELECT NOW()');

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,32 @@
 const { Router } = require("express");
+const pool = require("../dbPG");
 
 const router = Router();
 
-router.get("/task", (req, res) => {
-  res.send("retrieving a list of tasks");
+router.get('/task', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while querying the database' });
+  }
+});
+
+router.get("/task/10", (req, res) => {
+  res.send("retrieving a single tasks");
+});
+
+router.post("/task", (req, res) => {
+  res.send("creating a tasks");
+});
+
+router.delete("/task", (req, res) => {
+  res.send("deleting a tasks");
+});
+
+router.put("/task", (req, res) => {
+  res.send("updating a tasks");
 });
 
 module.exports = router;
