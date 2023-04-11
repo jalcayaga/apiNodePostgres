c3208fb3b74da1e1228ed3b4a4ddb74a20f4f01c
Javier Alcayaga <javier.alcayaga@subred.cl>
2023-04-11 02:34:57 -0400

    consultando una sola tarea ok.

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -4,8 +4,21 @@ const getAllTasks = async (req, res) => {
   res.send("Retrieven a list of task");
 };
 
-const getTask = (req, res) => {
-  res.send("Retrieven a single task");
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
+
+    if (result.rows.length === 0)
+      return res.status(404).json({
+        message: "Task not found",
+      });
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 const createTask = async (req, res) => {
